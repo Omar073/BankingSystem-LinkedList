359b06e54c6d067560c9b4b691b0e993c1c8be64
Omar073 <omarahmed7703@gmail.com>
2023-11-26 21:06:31 +0200

    made attributes private and created setters and getters

--- lib/Classes/Center.dart
+++ lib/Classes/Center.dart
@@ -1,15 +1,47 @@
 class Center {
-  String centerName;
-  String centerAddress;
-  String addressMapsLink;
-  String activeHours;
-  int centerCapacity;
+  String _centerName;
+  String _centerAddress;
+  String _addressMapsLink;
+  String _activeHours;
+  int _centerCapacity;
 
   Center({
-    required this.centerName,
-    required this.centerAddress,
-    required this.addressMapsLink,
-    required this.activeHours,
-    required this.centerCapacity,
-  });
+    required String centerName,
+    required String centerAddress,
+    required String addressMapsLink,
+    required String activeHours,
+    required int centerCapacity,
+  })  : _centerName = centerName,
+        _centerAddress = centerAddress,
+        _addressMapsLink = addressMapsLink,
+        _activeHours = activeHours,
+        _centerCapacity = centerCapacity;
+
+  // Getter methods
+  String get centerName => _centerName;
+  String get centerAddress => _centerAddress;
+  String get addressMapsLink => _addressMapsLink;
+  String get activeHours => _activeHours;
+  int get centerCapacity => _centerCapacity;
+
+  // Setter methods
+  set centerName(String value) {
+    _centerName = value;
+  }
+
+  set centerAddress(String value) {
+    _centerAddress = value;
+  }
+
+  set addressMapsLink(String value) {
+    _addressMapsLink = value;
+  }
+
+  set activeHours(String value) {
+    _activeHours = value;
+  }
+
+  set centerCapacity(int value) {
+    _centerCapacity = value;
+  }
 }
--- lib/Classes/Student.dart
+++ lib/Classes/Student.dart
@@ -1,25 +1,54 @@
 import 'User.dart';
 
 class Student extends User {
-  String year;
-  String College;
-  String Major;
-  // Date bdate;
-  String University;
-  // List<Course> registeredCourses;
+  // Private attributes
+  late int _year;
+  late String _college;
+  late String _major;
+  // late Date _bdate;
+  late String _university;
+  // late List<Course> _registeredCourses;
 
+  // Constructor with named parameters
   Student({
-    required super.name,
-    required super.ID,
-    required super.age,
-    required super.phonenum,
-    required super.email,
-    required super.password,
-    required this.year,
-    required this.College,
-    required this.Major,
-    // required this.bdate,
-    required this.University,
-    // required this.registeredCourses,
-  });
+    required String name,
+    required String ID,
+    required int age,
+    required String phonenum,
+    required String email,
+    required String password,
+    required String year,
+    required String college,
+    required String major,
+    // required Date bdate,
+    required String university,
+    // required List<Course> registeredCourses,
+  }) : super(
+          name: name,
+          ID: ID,
+          age: age,
+          phonenum: phonenum,
+          email: email,
+          password: password,
+  );
+
+  // Getters and setters for private attributes
+  // Getters and setters for private attributes
+  int get year => _year;
+  set year(int value) => _year = value;
+
+  String get college => _college;
+  set college(String value) => _college = value;
+
+  String get major => _major;
+  set major(String value) => _major = value;
+
+  // Date get bdate => _bdate;
+  // set bdate(Date value) => _bdate = value;
+
+  String get university => _university;
+  set university(String value) => _university = value;
+
+// List<Course> get registeredCourses => _registeredCourses;
+// set registeredCourses(List<Course> value) => _registeredCourses = value;
 }
