711b8c94acd788e8bb2e1fa28ca31e35ebebadc5
Omar073 <omarahmed7703@gmail.com>
2023-11-26 20:20:18 +0200

    center class

---
+++ lib/Classes/Center.dart
@@ -0,0 +1,15 @@
+class Center {
+  String centerName;
+  String centerAddress;
+  String addressMapsLink;
+  String activeHours;
+  int centerCapacity;
+
+  Center({
+    required this.centerName,
+    required this.centerAddress,
+    required this.addressMapsLink,
+    required this.activeHours,
+    required this.centerCapacity,
+  });
+}
