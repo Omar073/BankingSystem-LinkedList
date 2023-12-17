2cbbb68f3d9dc9a122412bf0c4daaff4aaca54ae
Mohamed-Ahmed-Esmat <21p0144@eng.asu.edu.eg>
2023-12-17 11:46:52 +0200

    Added the landing, login and signup pages
    Added what needed to develop these three pages, which includes adding the needed assets, adding the loading widget. It is not authenticated yet

--- lib/main.dart
+++ lib/main.dart
@@ -1,200 +1,36 @@
+import 'package:bankingsystem_linkedlist/pages/LandingPage.dart';
+import 'package:bankingsystem_linkedlist/pages/LoginPage.dart';
+import 'package:bankingsystem_linkedlist/pages/SignUp.dart';
 import 'package:flutter/material.dart';
-import 'Classes/Center.dart';
-import 'Classes/Course.dart';
-import 'Classes/Instructor.dart';
-import 'Classes/Schedule.dart';
-import 'Classes/Student.dart';
 
 void main() {
-  // runApp(const MyApp());
-
-  // Create a sample student
-  Student student = Student(
-    name: "John Doe",
-    ID: "S001",
-    age: 20,
-    phonenum: "123-456-7890",
-    email: "john.doe@example.com",
-    password: "password",
-    year: "Sophomore",
-    college: "Engineering",
-    major: "Computer Science",
-    university: "XYZ University",
-  );
-
-// Create a sample instructor
-  Instructor instructor = Instructor(
-    name: "Dr. Smith",
-    ID: "I001",
-    age: 35,
-    phonenum: "987-654-3210",
-    email: "smith@example.com",
-    password: "password",
-    assignedCourses: [], // Empty for now
-    isFullTime: true,
-  );
-
-// Create a sample center
-  myCenter center = myCenter(
-    centerName: "Tech Center",
-    centerAddress: "123 Tech Street, City",
-    addressMapsLink: "https://maps.example.com/tech-center",
-    activeHours: "9 AM - 6 PM",
-    centerCapacity: 100,
-  );
-
-// Create a sample schedule
-  Schedule schedule = Schedule(
-    courseDate: DateTime(2023, 11, 25),
-    frequencyPerWeek: 3,
-    time: "10:00 AM - 12:00 PM",
-  );
-
-// Create a sample course
-  Course course = Course(
-    courseID: "C001",
-    courseName: "Introduction to Programming",
-    courseDescription: "Learn the basics of programming.",
-    assignedStudents: [student],
-    assignedInstructor: instructor,
-    availableCenters: [center],
-    maxCapacity: 50,
-    startDate: DateTime(2023, 11, 20),
-    endDate: DateTime(2024, 1, 15),
-    prerequisites: ["None"],
-    isActive: true,
-    courseSchedule: schedule,
-  );
-
-// Add the course to the instructor's assigned courses
-  instructor.addCourse(course);
-
-// Add the course to the center's available courses
-  center.addCourseToCenter(course);
-
-// Print some information
-  print("Student Name: ${student.name}");
-  print("Instructor Name: ${instructor.name}");
-  print("Center Name: ${center.centerName}");
-  print("Course Name: ${course.courseName}");
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static final ValueNotifier<ThemeMode> themeNotifier =
+      ValueNotifier(ThemeMode.light);
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return ValueListenableBuilder<ThemeMode>(
+        valueListenable: themeNotifier,
+        builder: (_, ThemeMode currentMode, __) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            // theme: AppTheme().lightTheme,
+            // darkTheme: AppTheme().darkTheme,
+            themeMode: currentMode,
+            routes: {
+              '/sign_up': (context) => const SignUp(),
+              '/login': (context) => const LoginPage(),
+              //    '/profile': (context) => const ProfilePage(),
+            },
+            // home: const AuthCheck(),
+            home: const LandingPage(),
+          );
+        });
   }
 }
---
+++ lib/pages/LoginPage.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Color myColor = const Color.fromRGBO(179, 121, 223, 0.2);
+  var hidePass = true;
+  var passIcons = const Icon(Icons.visibility_off);
+  final emailCont = TextEditingController();
+  final passCont = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  bool loading = false;
+
+  Future<void> _handleLogin() async {
+    // Validate the form before proceeding
+    if (_formKey.currentState!.validate()) {
+      setState(() => loading = true);
+
+      // Your custom login logic goes here
+
+      // Dismiss the loading page
+      setState(() => loading = false);
+
+      // Handle the success or failure of login
+    }
+  }
+
+  Future<void> _handleGoogleLogin() async {
+    setState(() => loading = true);
+
+    // Your custom Google login logic goes here
+
+    // Dismiss the loading page
+    setState(() => loading = false);
+
+    // Handle the success or failure of Google login
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return loading
+        ? const CircularProgressIndicator()
+        : Scaffold(
+            body: SingleChildScrollView(
+              child: Center(
+                child: Column(
+                  children: [
+                    const SafeArea(
+                      child: SizedBox(
+                        width: 300,
+                        height: 200,
+                        child: Image(
+                          image: AssetImage(
+                              'assets/images/support, technology, error _ deadline, stress, man, customer service.png'),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                          topRight: Radius.circular(30),
+                        ),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Color(0x6082AFC4),
+                            Color(0x50B4E7FF),
+                          ],
+                        ),
+                      ),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 20),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 20),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                              child: Text(
+                                "Email",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                              child: TextFormField(
+                                validator: (val) =>
+                                    val!.isEmpty ? 'Enter your email' : null,
+                                controller: emailCont,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  hintText: 'Enter your email',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 40),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                              child: Text(
+                                "Password",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 0),
+                              child: TextFormField(
+                                validator: (val) =>
+                                    val!.isEmpty ? 'Enter your password' : null,
+                                obscureText: hidePass,
+                                controller: passCont,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  hintText: 'Enter your password',
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      hidePass = !hidePass;
+                                      passIcons = hidePass
+                                          ? const Icon(Icons.visibility_off)
+                                          : const Icon(Icons.visibility);
+                                      setState(() {});
+                                    },
+                                    icon: passIcons,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 5),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.fromLTRB(16.0, 0, 16.0, 0),
+                              child: TextButton(
+                                onPressed: () {
+                                  // Handle Forgot Password logic
+                                  // Navigator.of(context).push(
+                                  //   MaterialPageRoute(
+                                  //     builder: (context) => ForgotPasswordScreen(),
+                                  //   ),
+                                  // );
+                                },
+                                child: Text("Forgot Password?"),
+                              ),
+                            ),
+                            const SizedBox(height: 30),
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  child: MaterialButton(
+                                    onPressed: () async {
+                                      // Validate the form before logging in
+                                      await _handleLogin();
+                                    },
+                                    child: const Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          "Log in",
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      margin: const EdgeInsets.only(right: 8.0),
+                                      height:
+                                          1.0, // Adjust the height as needed
+                                      color: Colors
+                                          .grey[600], // Color of the dashes
+                                    ),
+                                  ),
+                                  Text(
+                                    "or",
+                                    style: TextStyle(
+                                      color: Colors.grey[800],
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      margin: const EdgeInsets.only(left: 8.0),
+                                      height:
+                                          1.0, // Adjust the height as needed
+                                      color: Colors
+                                          .grey[600], // Color of the dashes
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  padding: const EdgeInsets.all(8.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                      color:
+                                          Colors.grey.shade300, // Border color
+                                      width: 2.0, // Border width
+                                    ),
+                                    borderRadius: BorderRadius.circular(12.0),
+                                  ),
+                                  child: MaterialButton(
+                                    onPressed: () async {
+                                      setState(() => loading = true);
+                                      await _handleGoogleLogin();
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Container(
+                                          width: 50.0,
+                                          height: 50.0,
+                                          alignment: Alignment.center,
+                                          child: Image.asset(
+                                              "assets/images/google.png"),
+                                        ),
+                                        const Text(
+                                          "Login with Google",
+                                          style: TextStyle(fontSize: 18),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Don't have an account ?",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey[600],
+                                  ),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pushReplacementNamed(
+                                        context, '/sign_up');
+                                  },
+                                  child: const Text("Sign up"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+  }
+
+  @override
+  void dispose() {
+    emailCont.dispose();
+    passCont.dispose();
+    super.dispose();
+  }
+}
