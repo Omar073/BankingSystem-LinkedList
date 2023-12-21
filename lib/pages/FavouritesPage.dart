0982f1f7520c279d5a8bd0be9a7a3410d48598a3
kareem2002-k <kareemsamir166@gmail.com>
2023-12-21 23:21:45 +0200

    Favourites page done

--- lib/pages/FavouritesPage.dart
+++ lib/pages/FavouritesPage.dart
@@ -2,39 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../Classes/Course.dart';
 import '../Classes/Student.dart';
-import '../Classes/User.dart'; // Import your User class here
-import '../Providers/UserProvider.dart'; // Import your UserProvider class here
+import '../Classes/User.dart';
+import '../Providers/UserProvider.dart';
+import '../pages/CourseInfo.dart'; // Import your CourseInfo class
 
 class FavouritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Assuming you have a UserProvider that provides the current user
-    User? user = context.read<UserProvider>().user;
+    User? user = context.watch<UserProvider>().user;
 
     // Check if the user is a Student and has registered courses
     if (user is Student && user.registeredCourses.isNotEmpty) {
       List<Course> registeredCourses = user.registeredCourses;
 
       return Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                physics: NeverScrollableScrollPhysics(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'My Registered Courses',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
                 itemCount: registeredCourses.length,
                 itemBuilder: (context, index) {
                   final course = registeredCourses[index];
-                  // Build your UI using the course information
-                  return ListTile(
-                    title: Text(course.courseName),
-                    subtitle: Text(course.courseID),
-                    // Add more UI components as needed
+                  return Card(
+                    elevation: 5,
+                    margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        // Navigate to the CourseInfo page with the selected course
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CourseInfo(
+                              courseID: course.courseID,
+                              courseName: course.courseName,
+                              courseCode: course.courseID,
+                              isCourseFollowed: true, // You may adjust this based on your logic
+                            ),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              course.courseName,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Text(
+                              'Course ID: ${course.courseID}',
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                            // Add more UI components as needed
+                          ],
+                        ),
+                      ),
+                    ),
                   );
                 },
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
     } else {
